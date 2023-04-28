d27aaa207bff4a24e4fd2713f5881439f7b48452
Krishnam Soni <krishnamsoni07@gmail.com>
2023-04-28 23:50:37 +0530

    Added basic js functionality

--- javascript.js
+++ javascript.js
@@ -1,3 +1,31 @@
+let playerSelection;
+let computerSelection;
+let playerScore = 0,
+    computerScore = 0,
+    drawScore = 0;
+
+const rockButton = document.querySelector("#option_rock");
+rockButton.addEventListener("click", () => {
+    playerSelection = "Rock";
+    computerSelection = getComputerChoice();
+    let winner = whoWins(playerSelection, computerSelection);
+    updateHTML(playerSelection, computerSelection, winner);
+});
+const paperButton = document.querySelector("#option_paper");
+paperButton.addEventListener("click", () => {
+    playerSelection = "Paper";
+    computerSelection = getComputerChoice();
+    let winner = whoWins(playerSelection, computerSelection);
+    updateHTML(playerSelection, computerSelection, winner);
+});
+const scissorsButton = document.querySelector("#option_scissor");
+scissorsButton.addEventListener("click", () => {
+    playerSelection = "Scissors";
+    computerSelection = getComputerChoice();
+    let winner = whoWins(playerSelection, computerSelection);
+    updateHTML(playerSelection, computerSelection, winner);
+});
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     switch (randomNumber) {
@@ -10,57 +38,48 @@ function getComputerChoice() {
     }
 }
 
-function playerSelection() {
-    let input = prompt(
-        "Enter your choice (Rock, Paper, Scissors)"
-    ).toLowerCase();
-    switch (input) {
-        case "rock":
-            return "Rock";
-        case "paper":
-            return "Paper";
-        case "scissors":
-            return "Scissors";
-        default:
-            return "Invalid input";
-    }
-}
-
-let playerScore = 0,
-    computerScore = 0,
-    drawScore = 0,
-    invalidInput = 0;
-
-function round(computerSelection, playerSelection) {
-    console.log(
-        `Your choice: ${playerSelection}. \nComputer's choice: ${computerSelection}.`
-    );
-    if (playerSelection === "Invalid input") {
-        invalidInput++;
-        return "Invalid input.";
+function whoWins(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "draw";
     } else if (
         (playerSelection === "Rock" && computerSelection === "Scissors") ||
         (playerSelection === "Paper" && computerSelection === "Rock") ||
         (playerSelection === "Scissors" && computerSelection === "Paper")
     ) {
-        playerScore++;
-        return `You Win! ${playerSelection} beats ${computerSelection}.`;
-    } else if (playerSelection === computerSelection) {
-        drawScore++;
-        return "Draw!";
+        return "player";
     } else {
-        computerScore++;
-        return `You Lose! ${computerSelection} beats ${playerSelection}.`;
+        return "computer";
     }
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        console.log(round(getComputerChoice(), playerSelection()));
+function updateHTML(playerSelection, computerSelection, result) {
+    const computerImage = document.querySelector("#computer_choose img");
+    if (computerSelection === "Rock") {
+        computerImage.setAttribute("src", "img/rock.png");
+    } else if (computerSelection === "Paper") {
+        computerImage.setAttribute("src", "img/paper.png");
+    } else if (computerSelection === "Scissors") {
+        computerImage.setAttribute("src", "img/scissor.png");
     }
-    console.log(
-        `Computer score: ${computerScore}. \nYour score: ${playerScore}. \nDraws: ${drawScore}. \nInvalid inputs: ${invalidInput}.`
-    );
-}
 
-game();
+    const playerImage = document.querySelector("#player_choose img");
+    if (playerSelection === "Rock") {
+        playerImage.setAttribute("src", "img/rock.png");
+    } else if (playerSelection === "Paper") {
+        playerImage.setAttribute("src", "img/paper.png");
+    } else if (playerSelection === "Scissors") {
+        playerImage.setAttribute("src", "img/scissor.png");
+    }
+
+    if (result === "player") {
+        document.querySelector(
+            "#player"
+        ).textContent = `Player: ${++playerScore}`;
+    } else if (result === "computer") {
+        document.querySelector(
+            "#computer"
+        ).textContent = `Computer: ${++computerScore}`;
+    } else if (result === "draw") {
+        document.querySelector("#draw").textContent = `Draw: ${++drawScore}`;
+    }
+}
